706142eddb20370466a6ad5a5b5f6610940a33f2
hannut91 <hannut91@gmail.com>
2024-05-14 16:42:45 +0900

    제대로 동작하는지 확인할 수 있는 기본적인 기능을 추가하라
    동작하는  기능이 없으면 자바스크립트가 올바르게 동작하는지 확인할 수
    없습니다.
    그래서 버튼을 누르면 숫자가 증가하는 기본적인 기능을 추가합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((value) => value + 1);
+  }
+  
   return (
     <>
       <h1>Hell world</h1>
+
+      <div>
+        <p>카운트: {count}</p>
+        <button onClick={handleClick} type="button">클릭!</button>
+      </div>
     </>
   )
 }
